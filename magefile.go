779bf91f1f5105a2069bba79c8f17ef6a5cf8274
Alic Szecsei <aszecsei@gmail.com>
2018-10-02 23:09:46 -0500

    update magefile to perform ginkgo tests

--- magefile.go
+++ magefile.go
@@ -5,9 +5,10 @@ package main
 import (
 	"fmt"
 	"os"
-	"os/exec"
+	"path/filepath"
 
 	"github.com/magefile/mage/mg" // mg contains helpful utility functions, like Deps
+	"github.com/magefile/mage/sh"
 )
 
 // Default target to run when none is specified
@@ -18,19 +19,31 @@ import (
 func Build() error {
 	mg.Deps(InstallDeps)
 	fmt.Println("Building...")
-	cmd := exec.Command("go", "build", "-o", "catlang.exe", ".")
-	return cmd.Run()
+	return sh.RunV("go", "build", "-o", "catlang.exe", ".")
 }
 
 // Manage your deps, or running package managers.
 func InstallDeps() error {
 	fmt.Println("Installing Deps...")
-	cmd := exec.Command("go", "install")
-	return cmd.Run()
+	return sh.RunV("go", "install")
 }
 
 // Clean up after yourself
 func Clean() {
 	fmt.Println("Cleaning...")
 	os.RemoveAll("catlang.exe")
+}
+
+func Test() error {
+	mg.Deps(InstallDeps)
+	fmt.Println("Testing...")
+	if err := sh.RunV("go", "install", "github.com/onsi/ginkgo/ginkgo"); err != nil {
+		return err
+	}
+	gp := os.ExpandEnv("$GOPATH")
+	ginkgoPath := filepath.Join(gp, "bin", "ginkgo")
+	if err := sh.RunV(ginkgoPath, "-r", "-cover"); err != nil {
+		return err
+	}
+	return nil
 }
