8bf39d2a3a934415efc1a0b47d6fef1525534596
gh-actions <actions@github.com>
2021-03-30 00:57:41 +0000

    Deploy website - based on d6653f42bc52981d715fa984ff1f85133d03c81d

---
+++ assets/js/e4a1ca11.ce1918ca.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[36],{106:function(l){l.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Roadmap","permalink":"/catlang/blog/2018/12/16/roadmap"},{"title":"Hello, World!","permalink":"/catlang/blog/2018/10/17/hello-world"}]}')}}]);
